d0458d1b1686d73e41ffefff8a24771e2f530a98
Nikhil <103370224+Nikhil5409@users.noreply.github.com>
2026-08-07 23:50:38 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            int curr = nums[i];
+            if(hs.contains(curr)){
+                return true;
+            }
+            hs.add(curr);
+        }
+        return false;
+    }
+}
